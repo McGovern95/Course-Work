d363ea3603b883a4085858c269ec905aa2044239
McGovern95 <mcgovernchristian@gmail.com>
2018-02-12 16:18:52 -0700

    updated

--- NMSU-Computer Security/Project 1/Part 1/part1.cpp
+++ NMSU-Computer Security/Project 1/Part 1/part1.cpp
@@ -4,7 +4,9 @@
 #include <iostream>
 using namespace std;
 
-int frequency[26]; 
+int frequency[26][2]; 
+int key[] = {'E','T','A','O','I','N','S','H','R','D','L','C','U','M','W','F','G','Y','P',
+     'B','V','K','J','X','Q','Z'};
 
 int main(){
       
@@ -13,67 +15,74 @@ int main(){
      getline(file, s, (char) file.eof());
 
      cout << s << endl;
+
+     for(int i=0; i<26;i++){
+          frequency[i][1] = 'A' + i;
+
+     }
+
      //counts the frequency of letters
+     //
      for(int i=0;i<s.length();i++){
      	if (s.at(i) == 'A')
-     		{frequency[0]++;}
+     		{frequency[0][0]++;}
      	if (s.at(i) == 'B')
-     		{frequency[1]++;}
+     		{frequency[1][0]++;}
      	if (s.at(i) == 'C')
-     		{frequency[2]++;}
+     		{frequency[2][0]++;}
      	if (s.at(i) == 'D')
-     		{frequency[3]++;}
+     		{frequency[3][0]++;}
      	if (s.at(i) == 'E')
-     		{frequency[4]++;}
+     		{frequency[4][0]++;}
      	if (s.at(i) == 'F')
-     		{frequency[5]++;}
+     		{frequency[5][0]++;}
      	if (s.at(i) == 'G')
-     		{frequency[6]++;}
+     		{frequency[6][0]++;}
      	if (s.at(i) == 'H')
-     		{frequency[7]++;}
+     		{frequency[7][0]++;}
      	if (s.at(i) == 'I')
-     		{frequency[8]++;}
+     		{frequency[8][0]++;}
      	if (s.at(i) == 'J')
-     		{frequency[9]++;}
+     		{frequency[9][0]++;}
      	if (s.at(i) == 'K')
-     		{frequency[10]++;}
+     		{frequency[10][0]++;}
      	if (s.at(i) == 'L')
-     		{frequency[11]++;}
+     		{frequency[11][0]++;}
      	if (s.at(i) == 'M')
-     		{frequency[12]++;}
+     		{frequency[12][0]++;}
      	if (s.at(i) == 'N')
-     		{frequency[13]++;}
+     		{frequency[13][0]++;}
      	if (s.at(i) == 'O')
-     		{frequency[14]++;}
+     		{frequency[14][0]++;}
      	if (s.at(i) == 'P')
-     		{frequency[15]++;}
+     		{frequency[15][0]++;}
      	if (s.at(i) == 'Q')
-     		{frequency[16]++;}
+     		{frequency[16][0]++;}
      	if (s.at(i) == 'R')
-     		{frequency[17]++;}
+     		{frequency[17][0]++;}
      	if (s.at(i) == 'S')
-     		{frequency[18]++;}
+     		{frequency[18][0]++;}
      	if (s.at(i) == 'T')
-     		{frequency[19]++;}
+     		{frequency[19][0]++;}
      	if (s.at(i) == 'U')
-     		{frequency[20]++;}
+     		{frequency[20][0]++;}
      	if (s.at(i) == 'V')
-     		{frequency[21]++;}
+     		{frequency[21][0]++;}
      	if (s.at(i) == 'W')
-     		{frequency[22]++;}
+     		{frequency[22][0]++;}
      	if (s.at(i) == 'X')
-     		{frequency[23]++;}
+     		{frequency[23][0]++;}
      	if (s.at(i) == 'Y')
-     		{frequency[24]++;}
+     		{frequency[24][0]++;}
      	if (s.at(i) == 'Z')
-     		{frequency[25]++;}
+     		{frequency[25][0]++;}
      	
      }//end for
      //prints the frequency of letters
-     for(int j = 0;j<26;j++){
+     /*for(int j = 0;j<26;j++){
      	cout << (char)('A' + j) << ": ";
      	cout << frequency[j] << "" <<endl;
-     }
+     }*/
 
 
 
